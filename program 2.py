524e6520904ec04b106abf2f5989a356ef78e716
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-07-30 16:18:26 +0530

    program 2

---
+++ program 2.py
@@ -0,0 +1,14 @@
+print("Takshshila University")
+print("Ongur,Tindivanam,Villupuram dist.TN")
+print("-------------------------------------")
+print("STUDENT MARK LIST")
+print("------------------------------")
+str1=input("Enter t6he Registor no:")
+str2=input("Enter the Name:")
+python=int(input("python mark:"))
+DBMS=int(input("DBMS =mark:"))
+Accouting=int(input("Accouting mark:"))
+total=python+DBMS+Accouting
+print("Total Mark",total)
+Average=total/3
+print("Average Mark:",Average)
