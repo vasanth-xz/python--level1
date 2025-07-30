61c6c559e353a966e4a6842e14ecb58322b852e6
vasanth-xz <vasanthsaravanan357@gmail.com>
2025-07-30 16:16:40 +0530

    program 1

---
+++ program 1.py
@@ -0,0 +1,16 @@
+print("ARITHEMATIC OPERATION")
+print("----------------------")
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("result:")
+c=a+b
+print("Add:",c)
+c=a-b
+print("Sub:",c)
+c=a*b
+print("mul:",c)
+c=a/b
+print("Div:",c)
+c=a%b
+print("Mod:",c)
+      
